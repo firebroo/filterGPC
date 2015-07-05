9b54779ed9002f52ba529e6ac82c1f7aa606a026
firebroo <1121927992@qq.com>
2015-07-05 14:26:53 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -141,7 +141,7 @@ recurse_filter(HashTable *ht) {
             recurse_filter(isArray);
         }else {
             char *value = Z_STRVAL_PP(val);
-	        regex_filter(&value);
+	    regex_filter(&value);
             (*(zval**)head->pData)->value.str.val = value;
             (*(zval**)head->pData)->value.str.len = strlen(value);
         }
@@ -151,10 +151,10 @@ recurse_filter(HashTable *ht) {
 
 
 regex_filter(char **value){
-	int status, i;
+    int status, i;
     int cflags = REG_EXTENDED | REG_ICASE;
-	regmatch_t pmatch[1];
-	const size_t nmatch = 1;
+    regmatch_t pmatch[1];
+    const size_t nmatch = 1;
     regex_t reg;
     //it's a sample.
     const char *pattern = "shell_exec|passthru|system|exec|((\\.\\.[/\\])+)|select|sleep|benchmark|and|or|between";
