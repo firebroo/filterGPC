c8f144546967e9a8f80f05d59e6df6934064c10a
firebroo <1121927992@qq.com>
2015-11-22 12:07:00 +0800

    add php build-in function addslashes filter and use macros replace

--- hello.c
+++ hello.c
@@ -27,6 +27,7 @@
 #include "php.h"
 #include "php_ini.h"
 #include "ext/standard/info.h"
+#include "ext/standard/php_string.h"
 #include "php_hello.h"
 
 /* If you declare any globals in php_hello.h uncomment this:
@@ -35,40 +36,6 @@ ZEND_DECLARE_MODULE_GLOBALS(hello)
 
 /* True global resources - no need for thread safety here */
 static int le_hello;
-
-/* {{{ hello_functions[]
- *
- * Every user visible function must have an entry in hello_functions[].
- */
-const zend_function_entry hello_functions[] = {
-	PHP_FE(confirm_hello_compiled,	NULL)		/* For testing, remove later. */
-	PHP_FE_END	/* Must be the last line in hello_functions[] */
-};
-/* }}} */
-
-/* {{{ hello_module_entry
- */
-zend_module_entry hello_module_entry = {
-#if ZEND_MODULE_API_NO >= 20010901
-	STANDARD_MODULE_HEADER,
-#endif
-	"hello",
-	hello_functions,
-	PHP_MINIT(hello),
-	PHP_MSHUTDOWN(hello),
-	PHP_RINIT(hello),		/* Replace with NULL if there's nothing to do at request start */
-	PHP_RSHUTDOWN(hello),	/* Replace with NULL if there's nothing to do at request end */
-	PHP_MINFO(hello),
-#if ZEND_MODULE_API_NO >= 20010901
-	PHP_HELLO_VERSION,
-#endif
-	STANDARD_MODULE_PROPERTIES
-};
-/* }}} */
-
-#ifdef COMPILE_DL_HELLO
-ZEND_GET_MODULE(hello)
-#endif
 
 /* {{{ PHP_INI
  */
@@ -79,6 +46,34 @@ PHP_INI_BEGIN()
 PHP_INI_END()
 */
 /* }}} */
+
+/* Remove the following function when you have successfully modified config.m4
+   so that your module can be compiled into PHP, it exists only for testing
+   purposes. */
+
+/* Every user-visible function in PHP should document itself in the source */
+/* {{{ proto string confirm_hello_compiled(string arg)
+   Return a string to confirm that the module is compiled in */
+PHP_FUNCTION(confirm_hello_compiled)
+{
+	char *arg = NULL;
+	int arg_len, len;
+	char *strg;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &arg, &arg_len) == FAILURE) {
+		return;
+	}
+
+	len = spprintf(&strg, 0, "Congratulations! You have successfully modified ext/%.78s/config.m4. Module %.78s is now compiled into PHP.", "hello", arg);
+	RETURN_STRINGL(strg, len, 0);
+}
+/* }}} */
+/* The previous line is meant for vim and emacs, so it can correctly fold and
+   unfold functions in source code. See the corresponding marks just before
+   function definition, where the functions purpose is also documented. Please
+   follow this convention for the convenience of others editing your code.
+*/
+
 
 /* {{{ php_hello_init_globals
  */
@@ -116,11 +111,23 @@ PHP_MSHUTDOWN_FUNCTION(hello)
 /* Remove if there's nothing to do at request start */
 /* {{{ PHP_RINIT_FUNCTION
  */
-
 PHP_RINIT_FUNCTION(hello)
 {
     HashTable *ht = &EG(symbol_table);
+	/*需要过滤的变量数组*/
+	char* filter_arr[]= {"_GET", "_POST", "_COOKIE"};
+	int i;
     zval **val;
+	for(i=0; i<sizeof(filter_arr)/8; i++ ) {
+		if(zend_hash_find(ht, filter_arr[i], strlen(filter_arr[i])+1, (void**)&val) == SUCCESS) {
+			//recurse_filter(((*val)->value).ht);
+			if(Z_TYPE_P(*val) == IS_ARRAY) {
+				recurse_filter(Z_ARRVAL_PP(val));
+			}
+		}
+		val = NULL;
+	}
+	/*
     if(zend_hash_find(ht,"_GET",sizeof("_GET"),(void**)&val) == SUCCESS) {
         recurse_filter(((*val)->value).ht);
         //const char * value = (*((zval**)result->pListHead->pData))->value.str.val;
@@ -132,32 +139,71 @@ PHP_RINIT_FUNCTION(hello)
     if(zend_hash_find(ht,"_COOKIE",sizeof("_COOKIE"),(void**)&val) == SUCCESS) {
         recurse_filter(((*val)->value).ht);
     }
+	*/
     return SUCCESS;
 }
 
-int recurse_filter(HashTable *ht) {
+recurse_filter(HashTable *ht) {
     Bucket *head = ht->pListHead;
     zval **val;
     while(head) {
         val = (zval**)head->pData;
+		char *str_val;
+		/*
+		HashTable *isArray = ((*val)->value).ht;
         HashTable *isArray = ((*val)->value).ht;
         unsigned int nTableSize = isArray->nTableSize;
         unsigned int nTableMask = isArray->nTableMask;
         if (nTableSize - nTableMask == 1){
             recurse_filter(isArray);
-        }else {
+        }
+		*/
+		switch(Z_TYPE_PP(val)) {
+			/*如果hashtable为空，直接返回*/
+			case IS_NULL:
+				return;
+			/*为数组的时候，递归过滤*/
+			case IS_ARRAY:
+				//recurse_filter(((*val)->value).ht);
+				recurse_filter(Z_ARRVAL_PP(val));
+				break;
+			case IS_STRING:
+				str_val=Z_STRVAL_PP(val);
+				//单双引号过滤
+				str_val = php_addslashes(str_val, strlen(str_val), NULL, 0 TSRMLS_CC);
+				regex_filter(&str_val);
+				//(*(zval**)head->pData)->value.str.val = str_val;
+				//(*(zval**)head->pData)->value.str.len = strlen(str_val);
+				/*
+				Z_STRVAL_PP(val) = str_val;
+				Z_STRLEN_PP(val) = strlen(str_val);
+				*/
+				ZVAL_STRING(*val, str_val, 1);
+				break;
+			default:
+				break;
+		}
+		/*
+		if(Z_TYPE_P(*val) == IS_NULL) {
+			return ;
+		}
+	 	if(Z_TYPE_P(*val) == IS_ARRAY) {
+			HashTable *isArray = ((*val)->value).ht;
+            recurse_filter(isArray);
+        }else{
             char *value = Z_STRVAL_PP(val);
 	        regex_filter(&value);
             (*(zval**)head->pData)->value.str.val = value;
             (*(zval**)head->pData)->value.str.len = strlen(value);
         }
+		*/
         head = head->pListNext;
+		val = NULL;
     }
-	return SUCCESS;
 }
 
 
-int regex_filter(char **value){
+int regex_filter(char **str_val){
 	int status, i;
     int cflags = REG_EXTENDED | REG_ICASE;
 	regmatch_t pmatch[1];
@@ -167,9 +213,9 @@ int regex_filter(char **value){
     const char *pattern = "shell_exec|passthru|system|exec|((\\.\\.[/\\])+)|select|sleep|benchmark|and|or|between";
 
     regcomp(&reg, pattern, cflags);
-    status = regexec(&reg, *value, nmatch, pmatch, 0);
+    status = regexec(&reg, *str_val, nmatch, pmatch, 0);
     if (status == 0) {
-        strcpy(*value,"attack");
+        strcpy(*str_val,"attack");
     }
     regfree(&reg);
 	return SUCCESS;
@@ -199,34 +245,35 @@ PHP_MINFO_FUNCTION(hello)
 }
 /* }}} */
 
-
-/* Remove the following function when you have successfully modified config.m4
-   so that your module can be compiled into PHP, it exists only for testing
-   purposes. */
-
-/* Every user-visible function in PHP should document itself in the source */
-/* {{{ proto string confirm_hello_compiled(string arg)
-   Return a string to confirm that the module is compiled in */
-PHP_FUNCTION(confirm_hello_compiled)
-{
-	char *arg = NULL;
-	int arg_len, len;
-	char *strg;
-
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &arg, &arg_len) == FAILURE) {
-		return;
-	}
-
-	len = spprintf(&strg, 0, "Congratulations! You have successfully modified ext/%.78s/config.m4. Module %.78s is now compiled into PHP.", "hello", arg);
-	RETURN_STRINGL(strg, len, 0);
-}
+/* {{{ hello_functions[]
+ *
+ * Every user visible function must have an entry in hello_functions[].
+ */
+const zend_function_entry hello_functions[] = {
+	PHP_FE(confirm_hello_compiled,	NULL)		/* For testing, remove later. */
+	PHP_FE_END	/* Must be the last line in hello_functions[] */
+};
 /* }}} */
-/* The previous line is meant for vim and emacs, so it can correctly fold and
-   unfold functions in source code. See the corresponding marks just before
-   function definition, where the functions purpose is also documented. Please
-   follow this convention for the convenience of others editing your code.
-*/
 
+/* {{{ hello_module_entry
+ */
+zend_module_entry hello_module_entry = {
+	STANDARD_MODULE_HEADER,
+	"hello",
+	hello_functions,
+	PHP_MINIT(hello),
+	PHP_MSHUTDOWN(hello),
+	PHP_RINIT(hello),		/* Replace with NULL if there's nothing to do at request start */
+	PHP_RSHUTDOWN(hello),	/* Replace with NULL if there's nothing to do at request end */
+	PHP_MINFO(hello),
+	PHP_hello_VERSION,
+	STANDARD_MODULE_PROPERTIES
+};
+/* }}} */
+
+#ifdef COMPILE_DL_hello
+ZEND_GET_MODULE(hello)
+#endif
 
 /*
  * Local variables:
